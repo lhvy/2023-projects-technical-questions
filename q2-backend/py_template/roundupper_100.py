3fe6719b114a461ee9138a6c33fdaced94ad26d9
Lucas <me@lhvy.dev>
2023-01-07 19:50:15 +1100

    q2

--- q2-backend/py_template/roundupper_100.py
+++ q2-backend/py_template/roundupper_100.py
@@ -1,52 +1,99 @@
 from dataclasses import dataclass
 from enum import Enum
-from typing import Union, NamedTuple, List
+from typing import NamedTuple
 from flask import Flask, request
+from math import dist
+
+import sys
+
+# Please update to Python 3.11
+if sys.version_info >= (3, 11):
+    from typing import Self
+else:
+    from typing_extensions import Self
+
+# Location models a location in our amazing system
+@dataclass
+class Location:
+    x: int
+    y: int
+
+    def distance_to(self, other: Self) -> float:
+        return dist((self.x, self.y), (other.x, other.y))
+
 
 # SpaceCowboy models a cowboy in our super amazing system
 @dataclass
 class SpaceCowboy:
-    name: str
-    lassoLength: int
+    lasso_length: int
+    location: Location
+
+
+# SpaceAnimalType is an enum of all possible space animals we may encounter
+class SpaceAnimalType(str, Enum):
+    PIG = "pig"
+    COW = "cow"
+    FLYING_BURGER = "flying_burger"
+
 
 # SpaceAnimal models a single animal in our amazing system
 @dataclass
 class SpaceAnimal:
-    # SpaceAnimalType is an enum of all possible space animals we may encounter
-    class SpaceAnimalType(Enum):
-        PIG = "pig"
-        COW = "cow"
-        FLYING_BURGER = "flying_burger"
-
     type: SpaceAnimalType
-
-# SpaceEntity models an entity in the super amazing (ROUND UPPER 100) system
-@dataclass
-class SpaceEntity:
-    class Location(NamedTuple):
-        x: int
-        y: int
-
-    metadata: Union[SpaceCowboy, SpaceAnimal]
     location: Location
+
 
 # ==== HTTP Endpoint Stubs ====
 app = Flask(__name__)
-space_database: List[SpaceEntity] = []
+space_animals: list[SpaceAnimal] = []
+space_cowboys: dict[str, SpaceCowboy] = {}
 
 # the POST /entity endpoint adds an entity to your global space database
-@app.route('/entity', methods=['POST'])
+@app.route("/entity", methods=["POST"])
 def create_entity():
-    # TODO: implement me
-    ...
+    entities = request.get_json()["entities"]
 
-# lasooable returns all the space animals a space cowboy can lasso given their name
-@app.route('/lassoable', methods=['GET'])
+    for e in entities:
+        meta = e["metadata"]
+        location = Location(**e["location"])
+
+        match e["type"]:
+            case "space_cowboy":
+                # Not handling duplicate cowboy names as guaranteed to be unique
+                space_cowboys[meta["name"]] = SpaceCowboy(meta["lassoLength"], location)
+
+            case "space_animal":
+                match (meta["type"]):
+                    case "pig":
+                        type = SpaceAnimalType.PIG
+                    case "cow":
+                        type = SpaceAnimalType.COW
+                    case "flying_burger":
+                        type = SpaceAnimalType.FLYING_BURGER
+                    case _:
+                        return "Invalid animal type", 400
+                space_animals.append(SpaceAnimal(type, location))
+
+            case _:
+                return "Invalid entity type", 400
+
+    return "OK", 200
+
+
+# lassoable returns all the space animals a space cowboy can lasso given their name
+@app.route("/lassoable", methods=["GET"])
 def lassoable():
-    # TODO: implement me
-    ...
+    name = request.get_json()["cowboy_name"]
+    cowboy = space_cowboys[name]
+    lassoable = [
+        animal
+        for animal in space_animals
+        if cowboy.location.distance_to(animal.location) <= cowboy.lasso_length
+    ]
+
+    return {"space_animals": lassoable}, 200
 
 
 # DO NOT TOUCH ME, thanks :D
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True, port=8080)
--- q2-backend/ts_template/roundupper_100.ts
+++ q2-backend/ts_template/roundupper_100.ts
@@ -1,32 +1,64 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 // location is the simple (x, y) coordinates of an entity within the system
 // spaceCowboy models a cowboy in our super amazing system
 // spaceAnimal models a single animal in our amazing system
 type location = { x: number, y: number };
-type spaceCowboy = { name: string, lassoLength: number };
-type spaceAnimal = { type: "pig" | "cow" | "flying_burger" };
-
-// spaceEntity models an entity in the super amazing (ROUND UPPER 100) system
-type spaceEntity =
-    | { type: "space_cowboy", metadata: spaceCowboy, location: location }
-    | { type: "space_animal", metadata: spaceAnimal, location: location };
+type spaceCowboy = { lassoLength: number, location: location };
+type spaceAnimal = { type: "pig" | "cow" | "flying_burger", location: location };
 
 
 // === ADD YOUR CODE BELOW :D ===
 
 // === ExpressJS setup + Server setup ===
-const spaceDatabase = [] as spaceEntity[];
+const spaceAnimals = [] as spaceAnimal[];
+const spaceCowboys: { [name: string]: spaceCowboy } = {};
 const app = express();
+app.use(bodyParser.json());
 
 // the POST /entity endpoint adds an entity to your global space database
 app.post('/entity', (req, res) => {
-    // TODO: fill me in
+  const entities = req.body["entities"];
+
+  for (const entity of entities) {
+    const metadata = entity["metadata"];
+    const location: location = entity["location"];
+
+    switch (entity["type"]) {
+      case "space_cowboy":
+        spaceCowboys[metadata["name"]] = { lassoLength: metadata["lassoLength"], location };
+        break;
+
+      case "space_animal":
+        const type = metadata["type"];
+        if (!["pig", "cow", "flying_burger"].includes(type)) {
+          res.status(400).send("Invalid Space Animal");
+          return;
+        }
+        spaceAnimals.push({ type, location });
+        break;
+
+      default:
+        res.status(400).send("Invalid Entity Type");
+        return;
+    }
+  }
+
+  res.status(200).send("OK");
 });
 
-// lasooable returns all the space animals a space cowboy can lasso given their name
+// lassoable returns all the space animals a space cowboy can lasso given their name
 app.get('/lassoable', (req, res) => {
-    // TODO: fill me in
-})
+  const name = req.body["cowboy_name"];
+
+  const cowboy = spaceCowboys[name];
+  const space_animals = spaceAnimals.filter(animal => {
+    const distance = Math.sqrt(Math.pow(animal.location.x - cowboy.location.x, 2) + Math.pow(animal.location.y - cowboy.location.y, 2));
+    return distance <= cowboy.lassoLength;
+  });
+
+  res.status(200).send({ space_animals })
+});
 
 app.listen(8080);
